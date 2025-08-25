4bd70167cf45db84cbc62236d94162516184b348
osama mohamoud <wwwhdosama@gmail.com>
2025-08-25 20:56:47 +0300

    Auto commit: 2025-08-25 20:56:47

--- project 1.cpp
+++ project 1.cpp
@@ -26,17 +26,116 @@ void  back_to_menu() {
 }
 //////////////////////////////////////////////////////////////////////////////////
 
+// split line(from file)  into raw data (very imp )
+vector<string> split_string(string line , string delmi) {
 
+	vector<string> vdata;
+	short pos = 0; 
+	string sword;
+	 
+	while ((pos = line.find(delmi)) != string::npos) {
 
+		sword = line.substr(0, pos);
 
+		if (sword != "") vdata.push_back(sword);
 
+		line.erase(0, pos + delmi.length());
+	}
 
+	if (line != "") vdata.push_back(line);
 
+	return vdata;
 
+}
 
+//convert line of file into data filled in struct (return stdata which filled with data)
+stdata convert_line_into_record(string line) {
 
+	vector<string> dataSplited;
+	stdata data;
+	dataSplited = split_string(line, delmi);
+
+	data.account_number = dataSplited[0];
+	data.pin = dataSplited[1];
+	data.name= dataSplited[2];
+	data.phone =dataSplited[3];
+	data.account_balance = dataSplited[4];
+
+	return data;
+
+}
+
+//read file then push all data into vector of struct 
+vector<stdata> Vector_have_all_data(string path){
+	
+	vector<stdata> v_with_all_data;
+	fstream read;
+	read.open(path, ios::in); //read mode 
+
+	if (read.is_open()) {
+
+		string ld=" "; // line of data 
+		stdata data;
+		while (getline(read, ld)) {
+			if (ld != "") 
+			{
+
+            data = convert_line_into_record(ld); // fill empty struct with data after convert line into raw data
+			v_with_all_data.push_back(data); // push the data in vector 
+
+			}
+			
+		}
+	}
+	else {
+		cout << "file couldn't open!" << endl;
+		cout << "\a";
+		screen_color(red);
+	}
+	return v_with_all_data;
+}
+
+//print header for show client list
+void print_header(int num) {
+	cout << "\n\n";
+	cout << setw(35) << right << " CLIENTS LIST (" << num << ")\n\n";
+
+	cout << left;
+	cout << setw(15) << "Account Num"
+		<< setw(10) << "Pin"
+		<< setw(22) << "Name"
+		<< setw(18) << "Phone"
+		<< setw(12) << "Balance"
+		<< endl;
+
+	cout << string(77, '-') << endl; // ?? ???? ??? ????????
+}
+//print data 
+void printStruct(const stdata& data)
+{
+	cout << "| " << left
+		<< setw(13) << data.account_number << " | "
+		<< setw(8) << data.pin << " | "
+		<< setw(18) << data.name << " | "
+		<< setw(13) << data.phone << " | "
+		<< right
+		<< setw(10) << data.account_balance << " |"
+		<< endl;
+	cout << "+---------------+----------+--------------------+---------------+------------+" << endl;
+}
+
+// option [1] in main menu 
 void show_client_list() {
-	cout << "hello" << endl;
+
+	vector<stdata> vprint;
+	vprint = Vector_have_all_data(path);
+
+	print_header(vprint.size()); // print header
+	for (stdata& client : vprint) {
+
+		
+		printStruct(client); // print clients data 
+	}
 
 	
 }
@@ -98,7 +197,7 @@ void do_job_according_to_number(enOption option) {
 	switch (option) {
 	case enOption::showClientList:
 		show_client_list();
-		back_to_menu();
+		back_to_menu(); // to back to main menu again 
 		break;
 
 	case enOption::Exit:
